15a463a323729970dd2dbcce14a2ecb049d90de8
Shyamin Ayesh <me@shyamin.com>
2020-06-29 03:51:27 +0530

    get table schema

--- cmd/schema/status.go
+++ cmd/schema/status.go
@@ -2,8 +2,12 @@ package schema
 
 import (
 	"fmt"
+	"log"
+	"database/sql"
 	"pebble/config"
 	"github.com/spf13/cobra"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var Schema_Status_Command = &cobra.Command{
@@ -15,5 +19,47 @@ var Schema_Status_Command = &cobra.Command{
 var conf = config.Config()
 
 func run(cmd *cobra.Command, args []string) {
-	fmt.Println("schema::status")
+
+	type (
+		Schema struct {
+			Field			string
+			Type			string
+			Collation		sql.NullString
+			Null			string
+			Key				string
+			Default			sql.NullString
+			Extra			string
+			Privileges		string
+			Comment			string
+		}
+	)
+
+	/*
+		We have to initialize viper configuration
+		for future usage in the below sections.
+	*/
+	config_connection := conf.Sub("connection")
+
+	conuri := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s", config_connection.Get("user"), config_connection.Get("password"), config_connection.Get("host"), config_connection.Get("port"), config_connection.Get("name"))
+	db, err := sql.Open(config_connection.Get("driver").(string), conuri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	schema := Schema {}
+	err = db.QueryRow("SHOW FULL COLUMNS FROM address").Scan(&schema.Field, &schema.Type, &schema.Collation, &schema.Null, &schema.Key, &schema.Default, &schema.Extra, &schema.Privileges, &schema.Comment)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println( schema.Field )
+
+
 }
