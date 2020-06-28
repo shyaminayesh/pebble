dd5cac0bf9b22c87a81ad0656b2a3bb66e35b14f
Shyamin Ayesh <me@shyamin.com>
2020-06-28 15:52:35 +0530

    dispatch application

---
+++ cmd/app.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"os"
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var Command = &cobra.Command{
+	Use:   "pebble",
+	Short: "Pebble is a simple DB schema management system.",
+	Run: func(cmd *cobra.Command, args []string) {},
+}
+
+
+func Dispatch() {
+
+	/*
+		Add commands to the command stack of the
+		Cobra instance
+	*/
+	Command.AddCommand(Version_Command)
+	Command.AddCommand(Schema_Command)
+
+	if err := Command.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
