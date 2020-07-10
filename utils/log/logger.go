202b6dfdf3773c437e38c06f7eb441bef5e39fc9
Shyamin Ayesh <me@shyamin.com>
2020-07-11 04:03:49 +0530

    simple utility to log information to terminal

---
+++ utils/log/logger.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	"fmt"
+)
+
+func Println(color string, info string, msg string) {
+
+	// WHITE
+	if color == "white" {
+		fmt.Println("\033[36m" + info + "\033[0m" + msg)
+	}
+
+	// GREEN
+	if color == "green" {
+		fmt.Println("\033[32m" + info + "\033[0m" + msg)
+	}
+
+}
