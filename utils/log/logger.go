08a83a98309f7823257572dd35e216469767efd5
Shyamin Ayesh <me@shyamin.com>
2020-07-11 04:17:01 +0530

    red color logging

--- utils/log/logger.go
+++ utils/log/logger.go
@@ -16,4 +16,8 @@ func Println(color string, info string, msg string) {
 		fmt.Println("\033[32m" + info + "\033[0m" + msg)
 	}
 
+	if color == "red" {
+		fmt.Println("\033[31m" + info + "\033[0m" + msg)
+	}
+
 }
