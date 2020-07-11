e4752da61c6839cd70dd51079e8f75ef7b8df58b
Shyamin Ayesh <me@shyamin.com>
2020-07-11 21:54:24 +0530

    skip processing table if not found

--- cmd/seed/seed.go
+++ cmd/seed/seed.go
@@ -43,40 +43,42 @@ func seed(cmd *cobra.Command, args []string) {
 		We have to read all the seed files and then
 		act according to the file instructions.
 	*/
-	var yamls []string
 	files, err := ioutil.ReadDir("./" + seed_configs.Get("dir").(string))
 	if err != nil {
 		logger.Println("red", "[FATAL]: ", "Failed to load seed files.")
 	}
 	for _, file := range files {
-		if strings.Contains(file.Name(), ".yml") {
-			yamls = append(yamls, strings.Replace(file.Name(), ".yml", "", -1))
-		}
-	}
-
-
-	/*
-		Iterate through each yaml file to apply
-		the changes one by one to the database.
-	*/
-	for _, yaml := range yamls {
 
 		/*
-			Check if the database table is already exists with the
-			provided yaml file name before inserting any records.
-			This will avoid popping errors by the database.
+			Iterate through each yaml file to apply
+			the changes one by one to the database.
 		*/
-		var table_count int
-		query := fmt.Sprintf("SELECT CAST(COUNT(TABLE_NAME) AS UNSIGNED) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='%s' AND TABLE_NAME='%s'", connection_configs.Get("name"), yaml)
-		db.QueryRow(query).Scan(&table_count)
-		if table_count == 0 {
-			logger.Println("red", "[FATAL]: ", "TABLE NOT FOUND ( " + yaml + " )")
-			os.Exit(1)
-		}
+		if strings.Contains(file.Name(), ".yml") {
+			file_name := strings.Replace(file.Name(), ".yml", "", -1)
 
-		fmt.Println( yaml )
+			/*
+				Check if the database table is already exists with the
+				provided yaml file name before inserting any records.
+				This will avoid popping errors by the database.
+			*/
+			var table_count int
+			query := fmt.Sprintf("SELECT CAST(COUNT(TABLE_NAME) AS UNSIGNED) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='%s' AND TABLE_NAME='%s'", connection_configs.Get("name"), file_name)
+			db.QueryRow(query).Scan(&table_count)
+			if table_count == 0 {
+				logger.Println("red", "[TABLE]: ", "SKIPPING [" + file_name + "]")
+			}
+
+			if table_count == 1 {
+
+				/*
+					Seems like we have a table for the yaml file that contain
+					seed data. At this point we can safely continue the process.
+				*/
+				logger.Println("green", "[TABLE]: ", "PROCESSING [" + file_name + "]")
+
+			}
+
+		}
 	}
 
-	// print
-	fmt.Println( yamls )
 }
