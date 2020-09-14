5e0eebb7e13b5c72efefa11cf1147c8d214ce259
Shyamin Ayesh <me@shyamin.com>
2020-09-14 22:26:01 +0530

    increment version number to v2.0.0

--- cmd/version.go
+++ cmd/version.go
@@ -9,6 +9,6 @@ var Version_Command = &cobra.Command{
 	Use:   "version",
 	Short: "Show version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v1.0.0-beta")
+		fmt.Println("v2.0.0")
 	},
 }
