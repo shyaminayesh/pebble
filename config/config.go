3aabf28f4fbc9e02b05eeb9e72044a28696e1f6c
Shyamin Ayesh <me@shyamin.com>
2020-10-07 22:37:33 +0530

    use yaml parser to parse main configuration

--- config/config.go
+++ config/config.go
@@ -2,8 +2,61 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"io/ioutil"
+	"gopkg.in/yaml.v3"
 	"github.com/spf13/viper"
 )
+
+
+type (
+	Configuration struct {
+		Connection	Connection
+		Schema		Schema
+		Backup		Backup
+	}
+
+	Connection struct {
+		Host		string		`yaml:"host"`
+		Port		uint		`yaml:"port"`
+		Database	string		`yaml:"database"`
+		User		string		`yaml:"user"`
+		Password	string		`yaml:"password"`
+		Dialect		string		`yaml:"dialect"`
+	}
+
+	Schema struct {
+		Directory	string		`yaml:"directory"`
+	}
+
+	Backup struct {
+		Directory	string		`yaml:"directory"`
+	}
+)
+
+
+func Get() Configuration {
+
+	/**
+	* We need to read the file and append the configuration
+	* properties to the Configuration struct to continue
+	* the configuration file parse
+	*/
+	buffer, err := ioutil.ReadFile("./pebble.yml")
+	if err != nil { log.Fatal(err) }
+
+	/**
+	* We have to file ready to parse using the yaml
+	* parser. We can return the Configuration struct
+	* once we complete parse.
+	*/
+	configuration := Configuration {}
+	yaml.Unmarshal(buffer, &configuration)
+	fmt.Println( configuration )
+	return configuration
+
+}
+
 
 func Config() *viper.Viper {
 
