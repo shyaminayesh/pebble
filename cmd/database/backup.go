a1f7280e6f4ef7ecfc934af8d955a0f2940c5a6b
Shyamin Ayesh <me@shyamin.com>
2020-10-08 01:21:24 +0530

    partially complted mysql backup mechanisam

--- cmd/database/backup.go
+++ cmd/database/backup.go
@@ -2,6 +2,10 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"database/sql"
+	"pebble/config"
+	"pebble/utils/dumper"
 	"github.com/spf13/cobra"
 )
 
@@ -13,5 +17,32 @@ var Database_Backup_Command = &cobra.Command{
 
 
 func run_backup(cmd *cobra.Command, args []string) {
-	fmt.Println("run_backup")
+
+	/**
+	* We have to load main pebble configuration to get
+	* essential configuration information to continue
+	* the process.
+	*/
+	Config := config.Get()
+
+
+	/**
+	* Establish connection to the database using the
+	* pebble configuration.
+	*/
+	dialect := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s", Config.Connection.User, Config.Connection.Password, Config.Connection.Host, Config.Connection.Port, Config.Connection.Database)
+	db, err := sql.Open(Config.Connection.Dialect, dialect)
+	if err != nil { log.Fatal(err) }
+	defer db.Close()
+
+	/**
+	* We have to Construct new instance of SQL dumper to
+	* continue the backup process of the database using
+	* export method.
+	*/
+	dumper, err := dumper.Construct(db)
+	if err != nil { log.Fatal(err) }
+
+	fmt.Println( dumper.Export() )
+
 }
